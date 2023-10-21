153f7aa920e13231eeb067bc3fb5948c3cbf8050
Taeuk Kang <code@taeuk.net>
2023-10-21 00:18:45 -0700

    Add today's visit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import prisma from "@/lib/prisma";
+
+async function getTodaysVisit() {
+  const todayAtMidnight = new Date();
+  todayAtMidnight.setHours(0, 0, 0, 0);
+
+  const visit = await prisma.gymVisit.findFirst({
+    where: {
+      checkIn: {
+        gte: todayAtMidnight,
+      },
+    },
+  });
+
+  return visit;
+}
+
+export default async function Home() {
+  const visit = await getTodaysVisit();
   return (
     <main>
-      <div>hello</div>
+      <div className="flex flex-col items-center text-center">
+        <div className="text-5xl">Did Taeuk go to the gym today?</div>
+        <div>
+          {visit?.checkIn.toISOString()} at {visit?.address}
+        </div>
+      </div>
     </main>
   );
 }
